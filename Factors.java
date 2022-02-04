319ff0ec2d332d95ad5e290bb8f170d0b4ffbb0a
lokeshbs1994 <lokeshbs619@gmail.com>
2022-02-04 11:00:45 +0530

    Basic core program5

---
+++ Factors.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Factors {
+    public static void main(String[] args) {
+        System.out.println("Provide a number to find out its factor: ");
+        Scanner sc = new Scanner(System.in);//console
+        int factorNumber = sc.nextInt();
+        for (int i=2; i<=factorNumber; i++){
+            if(factorNumber%i == 0)
+                System.out.print(i +"\t");
+        }
+    }
+}
